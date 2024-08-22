8e0f4a505abe8548170c17d877d54e4437a3ac3d
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-08-22 11:21:42 +0700

    ..

--- lab11.cpp
+++ lab11.cpp
@@ -7,12 +7,22 @@ typedef struct Node
  int Age;
  struct Node *next;
 }PhoneBook;
-int main(){
-PhoneBook *head, *tail, *pt, *pt1, *pt2;   
+PhoneBook *head, *tail, *pt, *pt1, *pt2; 
+void list(){
+  if(head==0){cout<<"Empty"<<endl;return;}
+  pt=head; 
+  while(pt->next !=0){cout<<"node address "<<pt <<" name "<<pt->Name <<" age "<<pt->Age
+  << " link node "<< pt->next<<endl; pt=pt->next;     }  
+  cout<<"node address "<<pt <<" name "<<pt->Name <<" age "<<pt->Age
+  << " link node "<< pt->next<<endl;
+}
+int main(){ 
 head=(PhoneBook*)  malloc(sizeof(PhoneBook));
 strcpy(head->Name,"John"); head->Age=21; head->next=0;
 tail=head;
-
-
+pt=(PhoneBook*) malloc(sizeof(PhoneBook));
+strcpy(pt->Name,"Jane");pt->Age=22;pt->next=0;
+tail->next=pt;  tail=pt;
+ list();
     return 0;
 }
