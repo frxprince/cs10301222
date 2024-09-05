2b03f02de0a61f61abbd2e5cf6837646f317a952
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-09-05 11:16:41 +0700

    ..

--- lab16.cpp
+++ lab16.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<stack>
+#include<windows.h>
+
 using namespace std;
 int main(){
-    string input="a= ((1+2)+(3*2)";
+    HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
+    string input="a= ((1+2)(3*2)";
     stack<char>S;
     cout<<input<<endl;
     for(int i=0;i<input.length();i++){
@@ -11,12 +14,17 @@ int main(){
         S.push('(');break;
         case ')':
         if(S.empty()){
-            cout<<"error"<<endl; return 0;
+            SetConsoleTextAttribute(hConsole,0x04);
+            cout<<")"<<endl;
+     SetConsoleTextAttribute(hConsole,0x0);        
+             return 0;
         } S.pop(); 
-     }
+     } cout<<input[i];
     }
     if(!S.empty()){
-        cout<< "Error"<<endl;
+           SetConsoleTextAttribute(hConsole,0x04);
+        cout<< ")"<<endl;
+           SetConsoleTextAttribute(hConsole,0x0); 
     }else{ cout<<" OK "<< endl;   }
     return 0;
 }
