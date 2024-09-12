948d2b8d6d5afe90d360b24e7071910435081c7e
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-09-12 09:52:21 +0700

    ..

--- lab17.cpp
+++ lab17.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 template<typename T> void show_bits(const T& a){
  const char* beg=reinterpret_cast<const char*>(&a);
- const char* end=beg+sizeof(a); 
- cout<<"binary"<<endl;
+ const char* end=beg+sizeof(a);
+cout<<"Intel binary"<<endl;
+while(beg!=end){
+    cout<<bitset<CHAR_BIT>(*beg++) <<" ";
+}
+cout<<endl;
+
+ cout<<"Non binary"<<endl;
  end--;
  for(int i=sizeof(a);i>0;i--){
     cout<<bitset<CHAR_BIT>(*end--)<<' ';
@@ -13,7 +19,7 @@ template<typename T> void show_bits(const T& a){
 } 
 
 int main(){
-    unsigned int X=0xff00; //1111 1111   0000 0000
+    unsigned int X=0x12345678; // 00010010 00110100 01010110 01111000
     cout<<X<< " --> ";
     show_bits(X);
     return 0;
