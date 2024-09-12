226fae201c4c5606444c4bd9396d38cb1d223ddd
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-09-12 10:05:45 +0700

    ...

--- lab17.cpp
+++ lab17.cpp
@@ -4,13 +4,13 @@ using namespace std;
 template<typename T> void show_bits(const T& a){
  const char* beg=reinterpret_cast<const char*>(&a);
  const char* end=beg+sizeof(a);
-cout<<"Intel binary"<<endl;
+cout<<"Intel binary Little endian"<<endl;
 while(beg!=end){
     cout<<bitset<CHAR_BIT>(*beg++) <<" ";
 }
 cout<<endl;
 
- cout<<"Non binary"<<endl;
+ cout<<"Non binary Big endian"<<endl;
  end--;
  for(int i=sizeof(a);i>0;i--){
     cout<<bitset<CHAR_BIT>(*end--)<<' ';
@@ -19,7 +19,7 @@ cout<<endl;
 } 
 
 int main(){
-    unsigned int X=0x12345678; // 00010010 00110100 01010110 01111000
+    double X=-1; // 00010010 00110100 01010110 01111000
     cout<<X<< " --> ";
     show_bits(X);
     return 0;
