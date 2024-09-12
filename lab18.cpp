c3e49d6c977d3d06188edcceda4583812ede0b57
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-09-12 10:25:41 +0700

    ..

--- lab18.cpp
+++ lab18.cpp
@@ -13,5 +13,14 @@ int main()
     Precedent['^']=3;
     Precedent['*']=2;     Precedent['/']=2;
     Precedent['+']=1;     Precedent['-']=1;
+   string input="1+2+3*4+5*6";
+   string token,token_op;
+   unsigned int i;
+   for(i=0;i<input.length();i++){
+     if( (input[i]=='+') ||  (input[i]=='-') || (input[i]=='*') || (input[i]=='/') || (input[i]=='^') || (input[i]=='(') ||  (input[i]==')')){
+        cout<<input[i]<<" operator"<<endl;
+     }else
+        cout<<input[i]<<"operand"<<endl;
+   }  
     return 0;
 }
