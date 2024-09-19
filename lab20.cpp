b4c870e71728561f3ecb9cd28eabf33a64d9e4a5
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-09-19 09:50:55 +0700

    ..

--- lab20.cpp
+++ lab20.cpp
@@ -8,17 +8,18 @@ void RemoveAllElement(queue<string> Qi){
   Qi.pop();
  }   
 }
+void ShowQ(queue<string> Qx){
+ queue<string>Qi=Qx;   
+ while(!Qi.empty()){
+  cout<<Qi.front()<< ",";
+  Qi.pop();
+ }   
+}
 int main(){
     queue<string> Q;
     Q.push("Hello"); Q.push("World");Q.push("MJU");
-
-    cout<<Q.front()<<endl;
-    Q.pop();  
-    cout<<Q.front()<<endl;
-    Q.pop(); 
-    cout<<Q.front()<<endl;
-    Q.pop();
-    RemoveAllElement(Q); 
+    //RemoveAllElement(Q);
+    ShowQ(Q);
     cout<<Q.size()<<endl;
     return 0;
 }
