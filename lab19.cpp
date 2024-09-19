2ac58418d07a219242567d2ce6c49d615cb4636d
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-09-19 09:22:56 +0700

    ..

---
+++ lab19.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+const int N=10;
+int Q[N];
+int front=0, rear=-1;
+
+void add(int x){
+   if(rear<N-1){
+   rear++;
+   Q[rear]=x;
+   }else{
+    cout<< "Overflow" << endl;
+   }
+}
+int dequeue(){
+    if(front<=rear){
+    int data=Q[front];
+    front++;
+    return data;
+    }else{
+        cout<<"Underflow"<<endl;
+        return -1;
+    }
+}
+
+int main(){
+    for(int i=0;i<20;i++)add(i);
+    return 0;
+}
